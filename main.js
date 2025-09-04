7d2c67c969555413c2dd6d21204c68c2675a3475
Josh <joshua.e.green2023@gmail.com>
2025-09-04 13:56:27 -0400

    Function corrections needed to lines 26 - 67 (variables are defined globally but somehow not called in function)

--- main.js
+++ main.js
@@ -22,3 +22,46 @@ function animate() {
 
   renderer.render( scene, camera );
 }
+
+//Creates Audio Analyser and adds it to the camera
+
+const listener = new THREE.AudioListener();
+camera.add ( listener ); 
+
+// creates audio source
+
+const sound = new THREE.Audio(listener);
+
+const audioLoader = new THREE.AudioLoader();
+audopLoader.load('sounds/ambient.ogg', function(buffer){
+  sound.setBuffer(buffer);
+  sound.setLoop(true);
+  sound.setVolume(0.5);
+  sound.play();
+});
+
+const analyser = new THREE.AudioAnalyser( sound, 32 );
+
+// gets the average frequency of the sound
+const data = analyser.getAverageFrequency();
+
+//! sets up the anayliser to gather the frequency data from the mp3 file
+
+const setupAudioContext = () => {
+  AudioContext = new window.AudioContext();
+  document.getElementById("myAudio").addEventListener("play", setupAudioContext);
+  source = audioContext.createMediaElementSource(audioElement);
+  analyser = audioContext.createAnalyser();
+  source.connect(analyser);
+  analyser.connect(audioContext.destination);
+  analyser.fftSize = 1024;
+  dataArray = new Uint8Array(analyser.freqencyBinCount);
+};
+
+const render = () => {
+  analyser.getByteFrequencyData(dataArray);
+
+  requestAnimationFrame(render);
+};
+
+render();
